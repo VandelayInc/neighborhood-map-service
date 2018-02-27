fc6464a02f33261678e4777ee6c80be3716ed00f
wyattfry <wyattfry@gmail.com>
2018-02-26 18:30:27 -0800

    made recommended changes (removed cruft)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import Description from './components/Description.jsx';
-import { withGoogleMap, GoogleMap, Marker} from 'react-google-maps';
-import NeighborhoodMap from './components/NeighborhoodMap.jsx';
-import dummyData from './dummy_data.js';
-import axios from 'axios';
 
-const MapWithAMarker = withGoogleMap(props => 
-  <GoogleMap
-    defaultZoom={14}
-    defaultCenter={{lat:0, lng:0}}>
-    <Marker position={{lat:0,lng:0}} />
-  </GoogleMap>
-);
+import Description from './components/Description';
+import NeighborhoodMap from './components/NeighborhoodMap';
+import dummyData from './dummy_data';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      listing: dummyData.listing
+      listing: dummyData.listing,
     };
-
-    axios.get('/rooms/10812630/neighborhood')
-      .then((room) => {this.setState({listing: room.data.listing})})
-      .catch((err) => {console.error(err)});
   }
 
   render() {
@@ -30,13 +17,11 @@ class App extends React.Component {
       <div>
         <span>The Neighborhood</span>
         <Description listing={this.state.listing} />
-        <span>{this.state.listing.lat + ' ' + this.state.listing.lng}</span>
-        <NeighborhoodMap listing={this.state.listing} />
+        <NeighborhoodMap lat={this.state.listing.lat} lng={this.state.listing.lng} />
         <span>Exact location information is provided after a booking is confirmed.</span>
       </div>
     );
   }
 }
-
 
 export default App;
--- client/src/components/NeighborhoodMap.jsx
+++ client/src/components/NeighborhoodMap.jsx
@@ -1,56 +1,34 @@
 import React from 'react';
 import axios from 'axios';
-import $ from 'jquery';
-import MAP_API_KEY from '../../../map_config.js';
+import MAP_API_KEY from '../../../map_config';
 
 class NeighborhoodMap extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: this.props.lat,
-      lng: this.props.lng
-    }
-    // setTimeout(()=>{this.showMap(this.state.lat, this.state.lng)},1000);
+  constructor() {
+    super();
+    this.showMap();
   }
-  showMap(latitude, longitude) {
-    axios.get('https://maps.googleapis.com/maps/api/js', {params: {key: MAP_API_KEY}})
-    .then(() => {
-      var uluru = {lat: this.state.lat, lng: this.state.lng};
-      var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 14,
-        center: uluru
+  showMap() {
+    axios.get('https://maps.googleapis.com/maps/api/js', { params: { key: MAP_API_KEY } })
+      .then(() => {
+        const uluru = { lat: this.props.lat, lng: this.props.lng };
+        const map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: uluru,
+        });
+        const marker = new google.maps.Marker({
+          position: uluru,
+          map,
+        });
+      })
+      .catch((err) => {
+        document.getElementById('map').innerHTML = 'Unable to load map', err;
       });
-      var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-      });
-    })
-    .catch(console.error);
   }
   render() {
     return (
-      <div id='map'></div>
+      <div id="map" />
     );
   }
 }
 
 export default NeighborhoodMap;
-
-// let showMap = () => {
-//   axios.get('https://maps.googleapis.com/maps/api/js', {params: {key: MAP_API_KEY}})
-//   .then(() => {
-//     var uluru = {lat: -25.363, lng: 131.044};
-//     var map = new google.maps.Map(document.getElementById('map'), {
-//       zoom: 4,
-//       center: uluru
-//     });
-//     var marker = new google.maps.Marker({
-//       position: uluru,
-//       map: map
-//     });
-//   })
-//   .catch(console.error);
-// };
-
-
-// showMap();
