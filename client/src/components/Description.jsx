ef591a9055af10f1333f8b72c11783b386455c7b
wyattfry <wyattfry@gmail.com>
2018-02-28 18:08:21 -0800

    applied DRY

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -12,32 +12,27 @@ class Description extends React.Component {
     this.setState({showMoreInfo: !this.state.showMoreInfo});
   }
   render() {
-    if (this.state.showMoreInfo) {
-      return (
-        <div>
-          <p>{ this.props.listing.primary_host.first_name }'s home is located in {this.props.listing.city}
-          , {this.props.listing.state}, {this.props.listing.country}. </p>
-          <p>{ this.props.listing.neighborhood_overview }</p>
-          
-          <div className='div-neighborhood-more-info'>
-            <span className='div-neighborhood-subheading'>Getting Around</span>
-            <p>{this.props.listing.transit}</p>
-            <span className='div-neighborhood-clickable' onClick={this.toggleShowMore}>Hide &uarr;</span>
-          </div>
+    return (
+      <div>
+        <p>{ this.props.listing.primary_host.first_name }'s home is located in {this.props.listing.city}
+        , {this.props.listing.state}, {this.props.listing.country}. </p>
+        <p>{ this.props.listing.neighborhood_overview }</p>
+        <div className='div-neighborhood-more-info'>
+          {this.state.showMoreInfo ?
+            <div>
+              <span className='div-neighborhood-subheading'>Getting Around</span>
+              <p>{this.props.listing.transit}</p>
+              <span className='div-neighborhood-clickable'
+                onClick={this.toggleShowMore}>Hide &uarr;
+              </span>
+            </div> : 
+            <span className='div-neighborhood-clickable'
+              onClick={this.toggleShowMore}>Read more about the neighborhood &darr;
+            </span>
+          }
         </div>
-      );
-    } else {
-      return (
-        <div>
-          <p>{ this.props.listing.primary_host.first_name }'s home is located in {this.props.listing.city}
-          , {this.props.listing.state}, {this.props.listing.country}. </p>
-          <p>{ this.props.listing.neighborhood_overview }</p>
-          <div className='div-neighborhood-more-info'>
-            <span className='div-neighborhood-clickable' onClick={this.toggleShowMore}>Read more about the neighborhood &darr;</span>
-          </div>
-        </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
