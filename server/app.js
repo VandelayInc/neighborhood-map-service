d33512253326a4d6e63aa9f38579e5bc6123fe3e
wyattfry <wyattfry@gmail.com>
2018-03-01 12:22:02 -0800

    implemented test for incorrect listing number

--- server/app.js
+++ server/app.js
@@ -5,14 +5,13 @@ const app = express();
 app.use(express.static(`${__dirname}/../client/dist`));
 
 app.get('/', (req, res) => {
-  console.log('Received GET request');
   res.status(200).end();
 });
 
 app.get('/api/:roomid', (req, res) => {
   console.log(`API: GET request for room ${req.params.roomid}`);
   db.findOne(req.params.roomid, (err, data) => {
-    if (err) {
+    if (err || data === null) {
       res.status(500).end();
     } else {
       res.json(data);
