5b948a1d608d2a5b33b79460cd1233828f8333b7
Steve Hallam <StevenAHallam@gmail.com>
2018-03-21 13:33:33 -0700

    Basic MySQL integration.

---
+++ db/indexMDB.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+let remoteUser = 'remote';
+let remotePwd = 'remote';
+let remoteServer = '54.219.166.224';
+
+const dbURI = `mongodb://${remoteUser}:${remotePwd}@${remoteServer}:27017/VandelayInc`;
+mongoose.connect(dbURI);
+
+const neighborhoodSchema = mongoose.Schema({
+  _id: Number,
+  listing: {
+    primary_host: { first_name: String },
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+    neighborhood_overview: String,
+    transit: String,
+    lat: Number,
+    lng: Number,
+  }
+});
+
+const Neighborhood = mongoose.model('Neighborhood', neighborhoodSchema);
+
+function insertMany(array, callback) {
+  Neighborhood.insertMany(array, callback);
+}
+function insertOne(room, callback) {
+  Neighborhood.create(room, callback);
+}
+function findOne(id, callback) {
+  Neighborhood.findOne().where('_id').equals(id).exec(callback);
+}
+function close() {
+  mongoose.close();
+}
+exports.insertOne = insertOne;
+exports.insertMany = insertMany;
+exports.findOne = findOne;
+exports.close = close;
