bf241a27317e7af545353d5dc933be268ae74953
Steve Hallam <StevenAHallam@gmail.com>
2018-03-26 14:23:55 -0700

    Implemented Redis.

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
-const nr = require('newrelic');
+// const newrelic = require('newrelic');
 const express = require('express');
+const redis = require('redis');
 const db = require('../db/indexMDB.js'); //Uncomment this to use MongoDB
 // const db = require('../db/indexSQL.js'); //Uncomment this to use MySQL
-
 const app = express();
+const client = redis.createClient();
+
+// app.locals.newrelic = newrelic;
+
 app.use('/rooms/:roomid/neighborhood', express.static(`${__dirname}/../client/dist`));
 app.use('/', express.static(`${__dirname}/../client/dist`));
 
@@ -14,13 +18,22 @@ app.use((req, res, next) => {
 });
 
 app.get('/api/neighborhood/:roomid', (req, res) => {
-  db.findOne(req.params.roomid, (err, data) => {
-    if (err || data === null) {
-      res.status(500).end();
-    } else {
-      res.json(data);
+  let id = req.params.roomid;
+  client.get(id, (error, data) => {
+    if (data) {
+      res.send(data);
     }
-  });
+    else {
+      db.findOne(id, (err, data) => {
+        if (err || data === null) {
+          res.status(500).end();
+        } else {
+          client.setex(id, 120, JSON.stringify(data));
+          res.json(data);
+        }
+      });
+    }
+  })
 });
 
 module.exports = app;
