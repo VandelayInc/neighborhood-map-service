7fc5016afd9c96e78fd6ebe7e62e77aca8230063
Steve Hallam <StevenAHallam@gmail.com>
2018-03-28 17:33:01 -0700

    SSR

--- db/vegetaWrite.js
+++ db/vegetaWrite.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const stream = fs.createWriteStream('/Users/stevehallam/go/src/vegeta/localWeightedModuleNuke.txt'); // absolute path to where you are running the vegeta go script. The txt file with all the ids to hit needs to be in there
+const stream = fs.createWriteStream('/Users/stevehallam/go/src/vegeta/hostedWeightedModuleNuke.txt'); // absolute path to where you are running the vegeta go script. The txt file with all the ids to hit needs to be in there
 
 const rand = (min, max) => (Math.random() * (max - min) + min);
 const generatePseudoRandom = (weightCenter, max) => {
@@ -17,13 +17,8 @@ const writer = (n = 1) => {
   let isReady = true;
   while (isReady && n < 3e6 + 1) {
     var randNum = generatePseudoRandom(0.85, 10000000);
-    var url = `GET http://localhost:3006/api/neighborhood/${randNum}`;
-    if (n !== 3e6 && n % 100) {
-      for (let i =0; i < 1000; i++) {
-        isReady = stream.write(`${url}\n`);
-      }
-      n += 1000;
-    } else if (n !== 3e6) {
+    var url = `GET http://54.193.59.19:3006/api/neighborhood/${randNum}`;
+    if (n !== 3e6) {
       isReady = stream.write(`${url}\n`);
     } else {
       isReady = stream.write(`${url}`);
